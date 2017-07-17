f1bd98857b74df3092469052ee49881c25f4bf40
Lykrast <aymericpm@gmail.com>
2017-07-17 14:22:34 +0200

    Added ore dictionnary entries
    (and a thanks for being able to use Painterly pack textures)

--- src/main/java/lykrast/defiledlands/common/init/ModRecipes.java
+++ src/main/java/lykrast/defiledlands/common/init/ModRecipes.java
@@ -1,10 +1,17 @@
 package lykrast.defiledlands.common.init;
 
 import lykrast.defiledlands.common.util.CorruptionRecipes;
+import net.minecraftforge.oredict.OreDictionary;
 
 public class ModRecipes {
 	
 	public static void init() {
+		OreDictionary.registerOre("stoneDefiled", ModBlocks.stoneDefiled);
+		OreDictionary.registerOre("dirtDefiled", ModBlocks.dirtDefiled);
+		OreDictionary.registerOre("sandstoneDefiled", ModBlocks.sandstoneDefiled);
+		OreDictionary.registerOre("treeWood", ModBlocks.tenebraLog);
+		OreDictionary.registerOre("plankWood", ModBlocks.tenebraPlanks);
+		
 		CorruptionRecipes.init();
 	}
 

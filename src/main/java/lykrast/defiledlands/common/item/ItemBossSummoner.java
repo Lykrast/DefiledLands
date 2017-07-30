a4c7ee41f7d19a8e41ca112e3ba7a2402ab400f0
Lykrast <aymericpm@gmail.com>
2017-07-30 16:05:23 +0200

    Added an advancement for summoning The Destroyer
    Even if it's still not survival summonable for now

--- src/main/java/lykrast/defiledlands/common/item/ItemBossSummoner.java
+++ src/main/java/lykrast/defiledlands/common/item/ItemBossSummoner.java
@@ -1,9 +1,10 @@
 package lykrast.defiledlands.common.item;
 
 import lykrast.defiledlands.common.block.BlockConjuringAltar;
-import lykrast.defiledlands.common.entity.boss.EntityDestroyer;
+import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumActionResult;
@@ -11,6 +12,7 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.Rotation;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.EnumDifficulty;
 import net.minecraft.world.World;
 
 public abstract class ItemBossSummoner extends Item {
@@ -30,12 +32,18 @@ public abstract class ItemBossSummoner extends Item {
         
         if (block instanceof BlockConjuringAltar)
         {
-        	if (((BlockConjuringAltar) block).isActive(worldIn, pos))
+        	if (((BlockConjuringAltar) block).isActive(worldIn, pos) && worldIn.getDifficulty() != EnumDifficulty.PEACEFUL)
         	{
         		if (!worldIn.isRemote)
         		{
         			Entity boss = getBoss(worldIn);
         			boss.setLocationAndAngles(pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, player.getRotatedYaw(Rotation.CLOCKWISE_180), 0.0F);
+
+                    for (EntityPlayerMP entityplayermp : worldIn.getEntitiesWithinAABB(EntityPlayerMP.class, boss.getEntityBoundingBox().grow(50.0D)))
+                    {
+                        CriteriaTriggers.SUMMONED_ENTITY.trigger(entityplayermp, boss);
+                    }
+                    
         			worldIn.spawnEntity(boss);
         		}
         		

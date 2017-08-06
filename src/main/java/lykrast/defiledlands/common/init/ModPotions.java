930cea0ba04d6d7f60000c070fe76a706faf7a61
Lykrast <aymericpm@gmail.com>
2017-08-06 14:50:21 +0200

    Added Grounded potion effect (for later)

--- src/main/java/lykrast/defiledlands/common/init/ModPotions.java
+++ src/main/java/lykrast/defiledlands/common/init/ModPotions.java
@@ -9,11 +9,12 @@ import net.minecraftforge.fml.common.registry.ForgeRegistries;
 
 public class ModPotions {
 	
-	public static Potion vulnerability;
+	public static Potion vulnerability, grounded;
 	
 	public static void init()
 	{
-		vulnerability = register(new PotionGeneric(true, 0x6f7689, 0), "vulnerability");
+		vulnerability = register(new PotionGeneric(true, 0x6f7689, 0, -1), "vulnerability");
+		grounded = register(new PotionGeneric(true, 0x5f5e57, 1, 0), "grounded");
 	}
 	
 	public static Potion register(Potion p, String name)
--- src/main/java/lykrast/defiledlands/common/potion/PotionGeneric.java
+++ src/main/java/lykrast/defiledlands/common/potion/PotionGeneric.java
@@ -1,16 +1,20 @@
 package lykrast.defiledlands.common.potion;
 
+import lykrast.defiledlands.common.init.ModPotions;
 import lykrast.defiledlands.core.DefiledLands;
 import net.minecraft.client.Minecraft;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.potion.Potion;
 import net.minecraft.util.ResourceLocation;
 
 public class PotionGeneric extends Potion {
 	protected static final ResourceLocation TEXTURE = new ResourceLocation(DefiledLands.MODID, "textures/gui/potion_effects.png");
+	protected int tickrate;
 
-	public PotionGeneric(boolean isBadEffectIn, int liquidColorIn, int icon) {
+	public PotionGeneric(boolean isBadEffectIn, int liquidColorIn, int icon, int rate) {
 		super(isBadEffectIn, liquidColorIn);
 		setIconIndex(icon % 8, icon / 8);
+		tickrate = rate;
 	}
 
 	@Override
@@ -19,5 +23,27 @@ public class PotionGeneric extends Potion {
 		Minecraft.getMinecraft().getTextureManager().bindTexture(TEXTURE);
 		return super.getStatusIconIndex();
 	}
+
+	@Override
+	public void performEffect(EntityLivingBase entityLivingBaseIn, int amplifier)
+    {
+		if (this == ModPotions.grounded)
+		{
+			if (!entityLivingBaseIn.onGround)
+			{
+				if (entityLivingBaseIn.motionY > 0) entityLivingBaseIn.motionY /= (double)(amplifier + 2);
+				entityLivingBaseIn.motionY -= 0.05D * (amplifier + 1);
+			}
+		}
+    }
+	
+	@Override
+    public boolean isReady(int duration, int amplifier)
+    {
+		if (tickrate < 0) return false;
+		
+		int k = tickrate >> amplifier;
+		return k <= 0 || duration % k == 0;
+    }
 
 }

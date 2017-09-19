9be4f80f1692628b53ad50678597e288f89d4dd4
Lykrast <aymericpm@gmail.com>
2017-09-19 21:01:51 +0200

    Bumped version to 1.0.0
    No big changes are actually planned, 0.3.0 was actually supposed to be the release version

--- src/main/java/lykrast/defiledlands/core/DefiledLands.java
+++ src/main/java/lykrast/defiledlands/core/DefiledLands.java
@@ -14,7 +14,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class DefiledLands {
 	
 	public static final String MODID = "defiledlands";
-    public static final String VERSION = "0.3.0";
+    public static final String VERSION = "1.0.0";
     
     @Instance(MODID)
     public static DefiledLands instance;

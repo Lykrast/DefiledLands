6efd7c7b4280ef6563adffcb724d2a180c1b6336
Lykrast <aymericpm@gmail.com>
2018-05-19 14:38:14 +0200

    Updated build script

--- src/main/java/lykrast/defiledlands/core/DefiledLands.java
+++ src/main/java/lykrast/defiledlands/core/DefiledLands.java
@@ -10,11 +10,12 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = DefiledLands.MODID, version = DefiledLands.VERSION, acceptedMinecraftVersions = "[1.12,1.13)")
+@Mod(modid = DefiledLands.MODID, name = DefiledLands.NAME, version = DefiledLands.VERSION, acceptedMinecraftVersions = "[1.12,1.13)")
 public class DefiledLands {
 	
 	public static final String MODID = "defiledlands";
-    public static final String VERSION = "1.3.0";
+	public static final String NAME = "Defiled Lands";
+    public static final String VERSION = "@VERSION@";
     
     @Instance(MODID)
     public static DefiledLands instance;

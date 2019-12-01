411e1b9a8250a15875446b64398c8d54fc4d55c7
Lykrast <aymericpm@gmail.com>
2019-12-01 15:28:49 +0100

    Fixed Tenebra Door dropping Oak Door

--- src/main/java/lykrast/defiledlands/common/block/BlockDoorGeneric.java
+++ src/main/java/lykrast/defiledlands/common/block/BlockDoorGeneric.java
@@ -1,10 +1,19 @@
 package lykrast.defiledlands.common.block;
 
+import java.util.Random;
+
+import lykrast.defiledlands.common.init.ModItems;
 import net.minecraft.block.BlockDoor;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.statemap.StateMap;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -13,11 +22,11 @@ public class BlockDoorGeneric extends BlockDoor implements ICustomItemBlock, ICu
 
 	public BlockDoorGeneric(Material material, SoundType soundType, float hardness, float resistance) {
 		super(material);
-        this.setHardness(hardness);
-        this.setResistance(resistance);
-        this.setSoundType(soundType);
+		this.setHardness(hardness);
+		this.setResistance(resistance);
+		this.setSoundType(soundType);
 	}
-	
+
 	public BlockDoorGeneric(Material material, SoundType soundType, float hardness, float resistance, String tool, int harvestLevel) {
 		this(material, soundType, hardness, resistance);
 		this.setHarvestLevel(tool, harvestLevel);
@@ -26,6 +35,16 @@ public class BlockDoorGeneric extends BlockDoor implements ICustomItemBlock, ICu
 	@Override
 	public ItemBlock getItemBlock() {
 		return null;
+	}
+
+	@Override
+	public ItemStack getItem(World worldIn, BlockPos pos, IBlockState state) {
+		return new ItemStack(ModItems.tenebraDoor);
+	}
+
+	@Override
+	public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+		return state.getValue(HALF) == BlockDoor.EnumDoorHalf.UPPER ? Items.AIR : ModItems.tenebraDoor;
 	}
 
 	@Override
